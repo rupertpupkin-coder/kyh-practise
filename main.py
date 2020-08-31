948f946b8ff4ef79a05ec77794af9410aac1c5c8
rupertpupkin-coder <70513094+rupertpupkin-coder@users.noreply.github.com>
2020-08-31 13:10:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Jag heter Jacob Nordlund och är uppvuxen i en liten by utanför Skellefteå")
+print("Jag har programmerat väldigt grundligt i gymnasiet 2008-2011. c++/c#")
+print("I flera år har jag letat efter en utbildning som lät rolig och fann till slut denna, yey!")
